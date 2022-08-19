b847f9cef7bb17032d379abf8be3eac8062bde89
Xifre Font <xifre.font.97@gmail.com>
2022-08-19 19:04:01 +0200

    Cors set up

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import "./loadEnvironment";
 import express from "express";
 import Debug from "debug";
 import chalk from "chalk";
+import cors from "cors";
 
 const debug = Debug("users:index");
 
 const app = express();
 const port = process.env.PORT ?? 4000;
+
+app.use(cors());
 
 app.listen(port, () => {
   debug(chalk.green(`Server listening on port ${port}`));
