9bf788992b0687c8bd945bc0de0a76b014d4f5be
Xifre Font <xifre.font.97@gmail.com>
2022-08-19 18:35:41 +0200

    Added audit, testing and sonar workflows

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/server/index.ts",
+    "!src/server/startServer.ts",
+  ],
 };
