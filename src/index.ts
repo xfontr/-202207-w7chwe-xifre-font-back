719c11a1c90450e544f354ed0691be1ea3cf3d37
Xifre Font <xifre.font.97@gmail.com>
2022-08-19 19:00:07 +0200

    Server listen function

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,8 @@ import chalk from "chalk";
 const debug = Debug("users:index");
 
 const app = express();
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(chalk.green(`Server listening on port ${port}`));
+});
