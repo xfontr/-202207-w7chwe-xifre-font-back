1a047c521f616c39883f24332f3e7afb5d1cc1f6
Xifre Font <xifre.font.97@gmail.com>
2022-08-19 22:31:53 +0200

    Tested sign up controller

---
+++ src/controllers/usersControllers.test.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../database/models/User";
+import mockUser from "../mocks/mockUser";
+import signUp from "./usersControllers";
+
+jest.mock("../utils/auth", () => () => ({
+  hashCreate: () => jest.fn().mockReturnValue("#"),
+}));
+
+describe("Given a signUp function (controller)", () => {
+  const status = 200;
+
+  const req = {
+    body: mockUser,
+  } as Partial<Request>;
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+
+  const next = jest.fn() as NextFunction;
+
+  User.create = jest.fn().mockReturnValue(mockUser);
+
+  describe("When called with a request, a response and a next function as arguments", () => {
+    test("It should call status with a code of 200", async () => {
+      await signUp(req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+    });
+
+    test("It should respond with a new user as a body", async () => {
+      await signUp(req as Request, res as Response, next);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
