ce7c5f640223554a6830cfb98b8c78ac1bcb3f37
Xifre Font <xifre.font.97@gmail.com>
2022-08-20 15:53:04 +0200

    Created function to get user data by id

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { signUp, signIn } from "../../controllers/usersControllers";
+import { signUp, signIn, userData } from "../../controllers/usersControllers";
 
 const usersRouter = express.Router();
 
 usersRouter.post("/sign-up", signUp);
 usersRouter.post("/sign-in", signIn);
+
+usersRouter.get("/:id", userData);
 
 export default usersRouter;
