93f922520041440835f9dced9d3c94f66d6e3ba0
Xifre Font <xifre.font.97@gmail.com>
2022-08-21 11:52:50 +0200

    Added authentication to get routes

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -5,13 +5,14 @@ import {
   userData,
   allUsersData,
 } from "../../controllers/usersControllers";
+import authentication from "../../middlewares/authentication";
 
 const usersRouter = express.Router();
 
 usersRouter.post("/sign-up", signUp);
 usersRouter.post("/sign-in", signIn);
 
-usersRouter.get("/all", allUsersData);
-usersRouter.get("/:id", userData);
+usersRouter.get("/all", authentication, allUsersData);
+usersRouter.get("/:id", authentication, userData);
 
 export default usersRouter;
